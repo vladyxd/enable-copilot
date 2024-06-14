d82c96b1ff6e43d1abb17311cc5e5bd8d866bcc0
VLADY <41295778+vladyxd@users.noreply.github.com>
2024-06-14 12:04:01 +0300

    Create _enable.copilot.bat

---
+++ _enable.copilot.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+:: Query the registry value
+for /f "tokens=3" %%A in ('reg query HKCU\Software\Microsoft\Windows\Shell\Copilot\BingChat /v IsUserEligible') do (
+    set IsUserEligible=%%A
+)
+if "%IsUserEligible%"=="0x0" (
+    echo User is not eligible for Copilot. Enabling.
+    reg add HKCU\Software\Microsoft\Windows\Shell\Copilot\BingChat /v IsUserEligible /t REG_DWORD /d 1 /f :: Here it sets the isEligible value to 1
+    echo Copilot enabled. Check your taskbar settings. - Windows 11 only.
+    
+) else (
+    echo User is already eligible for Copilot. Exiting.
+)
+
+endlocal
